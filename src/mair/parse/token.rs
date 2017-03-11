1ada807e955c224685dd21dcc43731f0eac69dc4
uHOOCCOOHu <uHOOCCOOHu@users.noreply.github.com>
2017-03-11 17:04:20 +0800

    Add number literal parsing in tokenizer. Derive some traits for enums. Make macro match_head more flexible. Add lots of tests.

--- src/mair/parse/token.rs
+++ src/mair/parse/token.rs
@@ -8,24 +8,25 @@ use self::LiteralType::*;
 
 type RawErr<'a> = TokenizeError<&'a [u8]>;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum TokenizeError<Pos=usize> {
     UnclosedComment(Pos),
     UnmatchedDelimiter(Pos, Pos),
     UnterminatedStr(Pos),
     InvalidRawStrBegin(Pos),
     InvalidEscape(Pos),
+    InvalidNumLit(Pos),
     UnexpectedChar(Pos),
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum DelimToken {
     Paren,
     Bracket,
     Brace,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Token<'a> {
     Delimited(DelimToken, Vec<Token<'a>>),
     InnerDoc(&'a str),
@@ -36,12 +37,13 @@ pub enum Token<'a> {
     Literal(LiteralType),
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum LiteralType {
     Char, Str, ByteStr,
     Int, Float,
     I8, I16, I32, I64, ISize,
     U8, U16, U32, U64, USize,
+    F32, F64,
 }
 
 /// Parse source string into a list of tokens.
@@ -64,6 +66,8 @@ pub fn tokenize(source: &str) -> Result<Vec<Token>, TokenizeError> {
                 Err(InvalidRawStrBegin(index_of(pos))),
             InvalidEscape(pos)           =>
                 Err(InvalidEscape(index_of(pos))),
+            InvalidNumLit(pos)           =>
+                Err(InvalidNumLit(index_of(pos))),
             UnexpectedChar(pos)          =>
                 Err(UnexpectedChar(index_of(pos))),
         },
@@ -71,27 +75,27 @@ pub fn tokenize(source: &str) -> Result<Vec<Token>, TokenizeError> {
 }
 
 macro_rules! match_head {
-    ($s:ident; _ => $e:expr;) => ($e);
-    ($s:ident; pat ($($pat:pat)|+) => $e:expr; $($tt:tt)*) => ({
+    ($s:expr; _ => $e:expr;) => ($e);
+    ($s:expr; pat ($($pat:pat)|+) => $e:expr; $($tt:tt)*) => ({
         match $s[0] {
             $($pat)|+ => $e,
             _         => match_head!($s; $($tt)*),
         }
     });
-    ($s:ident; $($pat:expr),+ => $e:expr; $($tt:tt)*) => ({
+    ($s:expr; $($pat:expr),+ => $e:expr; $($tt:tt)*) => ({
         if [$(&$pat[..]),+].iter().any(|p| $s.starts_with(p)) {
-            $e;
+            $e
         } else {
-            match_head!($s; $($tt)*);
+            match_head!($s; $($tt)*)
         }
     });
-    ($s:ident; $($pat:expr),+; $(($c:ident))* => $e:expr; $($tt:tt)*) => ({
+    ($s:expr; $($pat:expr),+; $(($c:ident))* => $e:expr; $($tt:tt)*) => ({
         if let Some(c) = [$(&$pat[..]),+].iter().find(|p| $s.starts_with(p)) {
             $(let $c = &$s[..c.len()];)*
             $s = &$s[c.len()..];
-            $e;
+            $e
         } else {
-            match_head!($s; $($tt)*);
+            match_head!($s; $($tt)*)
         }
     });
 }
@@ -115,13 +119,17 @@ fn tokens(mut s: &[u8]) -> Result<(&[u8], Vec<Token>), RawErr> {
             b"["        => delimited!(b']', Bracket);
             b"{"        => delimited!(b'}', Brace);
             b"//!" ;    => v.push(InnerDoc(line_comment_inner(&mut s)));
-            b"////";    => line_comment_inner(&mut s);
+            b"////";    => { line_comment_inner(&mut s); };
             b"///" ;    => v.push(OuterDoc(line_comment_inner(&mut s)));
-            b"//"  ;    => line_comment_inner(&mut s);
+            b"//"  ;    => { line_comment_inner(&mut s); };
             b"/*!" ;    => v.push(InnerDoc(block_comment_inner(&mut s)?));
-            b"/***";    => block_comment_inner(&mut s)?;
+            b"/***";    => { block_comment_inner(&mut s)?; };
             b"/**" ;    => v.push(OuterDoc(block_comment_inner(&mut s)?));
-            b"/*"  ;    => block_comment_inner(&mut s)?;
+            b"/*"  ;    => { block_comment_inner(&mut s)?; };
+            b"0x"  ;    => v.push(int_lit(&mut s, 16, true)?);
+            b"0o"  ;    => v.push(int_lit(&mut s, 8,  true)?);
+            b"0b"  ;    => v.push(int_lit(&mut s, 2,  true)?);
+            pat (b'0'...b'9') => v.push(num_lit(&mut s)?);
             b"\"", b"r\"", b"r#", b"b\"", b"br\"", b"br#" =>
                 v.push(string_lit(&mut s)?);
             b"::", b"->", b"=>",
@@ -147,6 +155,98 @@ fn tokens(mut s: &[u8]) -> Result<(&[u8], Vec<Token>), RawErr> {
         );
     }
     Ok((s, v))
+}
+
+/// Parse integer or float literal.
+#[inline]
+fn num_lit<'a>(s: &mut &'a [u8]) -> Result<Token<'a>, RawErr<'a>> {
+    let mut ns = *s;
+    let e = InvalidNumLit(ns);
+    macro_rules! consume_int { () => (
+        int_lit(&mut ns, 10, false).map_err(|_| e)?
+    ) };
+    int_lit(&mut ns, 10, false)?;
+    let mut ty = match_head!(ns;
+        b"._"            => return Err(e); // `1._2` in invalid
+        b"." ;           => { consume_int!(); Float };
+        _                => Int;
+    );
+    ty = match_head!(ns;
+        b"e", b"E" => {
+            ns = &ns[1..];
+            match_head!(ns;
+                b"-", b"+" => ns = &ns[1..]; // `1e-1`
+                _          => {};
+            );
+            consume_int!();
+            Float
+        };
+        _          => ty;
+    );
+    match_head!(ns;
+        b"i", b"u", b"f" => if let Some(nty) = num_suffix(&mut ns, true) {
+            if ty == Float && (nty != F32 && nty != F64) { // `1.2i32` is invalid
+                return Err(e);
+            }
+            ty = nty;
+        } else { return Err(e) };
+        _                => {};
+    );
+    *s = ns;
+    Ok(Literal(ty))
+}
+
+/// Parse integer literal.
+#[inline]
+fn int_lit<'a>(s: &mut &'a [u8], radix: u32, suffix: bool) ->
+        Result<Token<'a>, RawErr<'a>> {
+    assert!([2, 8, 10, 16].contains(&radix));
+    let mut ns = *s;
+    let mut ty = None;
+    while !ns.is_empty() { match ns[0] {
+        b'_' => ns = &ns[1..],
+        b'i' | b'u' if suffix => {
+            ty = ty.and_then(|_| num_suffix(&mut ns, false));
+            break;
+        },
+        c@b'0'...b'9'  =>
+            if (c as char).to_digit(radix).is_some() {
+                ty = Some(Int);
+                ns = &ns[1..];
+            } else { ty = None; break; },
+        b'a'...b'f' | b'A'...b'F' if radix == 16 => {
+            ty = Some(Int);
+            ns = &ns[1..];
+        },
+        _ => break,
+    }}
+    match ty {
+        Some(t) => { *s = ns; Ok(Literal(t)) },
+        None    => Err(InvalidNumLit(*s)),
+    }
+}
+
+/// Parse a number suffix.
+fn num_suffix<'a>(s: &mut &'a [u8], float: bool) -> Option<LiteralType> {
+    match_head!(*s;
+        b"i8"   ;   => Some(I8);
+        b"i16"  ;   => Some(I16);
+        b"i32"  ;   => Some(I32);
+        b"i64"  ;   => Some(I64);
+        b"isize";   => Some(ISize);
+        b"u8"   ;   => Some(U8);
+        b"u16"  ;   => Some(U16);
+        b"u32"  ;   => Some(U32);
+        b"u64"  ;   => Some(U64);
+        b"usize";   => Some(USize);
+        _           => if float {
+            match_head!(*s;
+                b"f32"; => Some(F32);
+                b"f64"; => Some(F64);
+                _       => None;
+            )
+        } else { None };
+    )
 }
 
 /// Parse an string literal ("" r"" b"" br"").
@@ -262,6 +362,63 @@ fn trim(s: &mut &[u8]) -> bool {
 /// A shortcut to `std::str::from_utf8_unchecked`
 #[inline]
 fn to_str(s: &[u8]) -> &str { unsafe{ from_utf8_unchecked(s) } }
+
+#[test]
+fn test_num_lit() {
+    let mut s: &[u8];
+    for &(c, rdx, suf, ref r, t) in [
+        (&b"_"[..],       10, true,  Err(InvalidNumLit(&b"_"[..])),      &b"_"[..]),
+        (&b"i32"[..],     10, true,  Err(InvalidNumLit(&b"i32"[..])),    &b"i32"[..]),
+        (&b"10_01."[..],  2,  true,  Ok(Literal(Int)),                   &b"."[..]),
+        (&b"1u32 "[..],   10, true,  Ok(Literal(U32)),                   &b" "[..]),
+        (&b"1u32 "[..],   10, false, Ok(Literal(Int)),                   &b"u32 "[..]),
+        (&b"_0_u9 "[..],  10, true,  Err(InvalidNumLit(&b"_0_u9 "[..])), &b"_0_u9 "[..]),
+        (&b"_0_u9 "[..],  10, false, Ok(Literal(Int)),                   &b"u9 "[..]),
+        (&b"0f64"[..],    10, true,  Ok(Literal(Int)),                   &b"f64"[..]),
+        (&b"678."[..],    10, true,  Ok(Literal(Int)),                   &b"."[..]),
+        (&b"678."[..],    8,  true,  Err(InvalidNumLit(&b"678."[..])),   &b"678."[..]),
+        (&b"1234 89"[..], 10, true,  Ok(Literal(Int)),                   &b" 89"[..]),
+    ].iter() {
+        println!("{} {} {} {:?} {}", to_str(c), rdx, suf, r, to_str(t));
+        s = c;
+        assert_eq!(int_lit(&mut s, rdx, suf), *r);
+        assert_eq!(s, t);
+    }
+    for &(c, ref r, t) in [
+        (&b"0"[..],         Ok(Literal(Int)),   &b""[..]),
+        (&b"0i8"[..],       Ok(Literal(I8)),    &b""[..]),
+        (&b"0i16"[..],      Ok(Literal(I16)),   &b""[..]),
+        (&b"0i32"[..],      Ok(Literal(I32)),   &b""[..]),
+        (&b"0i64"[..],      Ok(Literal(I64)),   &b""[..]),
+        (&b"0isize"[..],    Ok(Literal(ISize)), &b""[..]),
+        (&b"0u8"[..],       Ok(Literal(U8)),    &b""[..]),
+        (&b"0u16"[..],      Ok(Literal(U16)),   &b""[..]),
+        (&b"0u32"[..],      Ok(Literal(U32)),   &b""[..]),
+        (&b"0u64"[..],      Ok(Literal(U64)),   &b""[..]),
+        (&b"0usize"[..],    Ok(Literal(USize)), &b""[..]),
+        (&b"0f32"[..],      Ok(Literal(F32)),   &b""[..]),
+        (&b"0f64"[..],      Ok(Literal(F64)),   &b""[..]),
+        (&b"1.2"[..],       Ok(Literal(Float)), &b""[..]),
+        (&b"1e1"[..],       Ok(Literal(Float)), &b""[..]),
+        (&b"1e-1"[..],      Ok(Literal(Float)), &b""[..]),
+        (&b"1e+1"[..],      Ok(Literal(Float)), &b""[..]),
+        (&b"1.1e1"[..],     Ok(Literal(Float)), &b""[..]),
+        (&b"1.1e-1"[..],    Ok(Literal(Float)), &b""[..]),
+        (&b"1_.1_e-_1"[..], Ok(Literal(Float)), &b""[..]),
+        (&b"1.2f32"[..],    Ok(Literal(F32)),   &b""[..]),
+        (&b"1+"[..],        Ok(Literal(Int)),   &b"+"[..]),
+        (&b"1a32"[..],      Ok(Literal(Int)),   &b"a32"[..]),
+        (&b"1.2i32"[..],    Err(InvalidNumLit(&b"1.2i32"[..])), &b"1.2i32"[..]),
+        (&b"1e2i32"[..],    Err(InvalidNumLit(&b"1e2i32"[..])), &b"1e2i32"[..]),
+        (&b"1i65"[..],      Err(InvalidNumLit(&b"1i65"[..])),   &b"1i65"[..]),
+        (&b"1._2"[..],      Err(InvalidNumLit(&b"1._2"[..])),   &b"1._2"[..]),
+    ].iter() {
+        println!("{} {:?} {}", to_str(c), r, to_str(t));
+        s = c;
+        assert_eq!(num_lit(&mut s), *r);
+        assert_eq!(s, t);
+    }
+}
 
 #[test]
 fn test_string_lit() {
