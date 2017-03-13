92bf4e1504986e01a15cda60d618252b840ec2cb
uHOOCCOOHu <uHOOCCOOHu@users.noreply.github.com>
2017-03-13 23:11:36 +0800

    Add gen_position_map function. Add symbol `?` `@` parsing in tokenizer. Fix bug when parsing integer literal before symbol `..`. Add more tests for tokenizer.

--- src/mair/parse/mod.rs
+++ src/mair/parse/mod.rs
@@ -24,3 +24,33 @@ pub fn ptr_diff<T>(a: *const T, b: *const T) -> isize {
     use std::mem::size_of;
     (a as isize - b as isize) / size_of::<T>() as isize
 }
+
+/// Generate a map from character indices to line and column numbers.
+///
+/// # Example
+///
+/// ```
+/// use mair::parse::gen_position_map;
+/// let s1 = "ab\ncd\r\nde";
+/// let s2 = "\n!\r\n\n";
+/// assert_eq!(gen_position_map(s1), vec![
+///     (1, 1), (1, 2), (1, 3),
+///     (2, 1), (2, 2), (2, 3), (2, 4),
+///     (3, 1), (3, 2),
+/// ]);
+/// assert_eq!(gen_position_map(s2), vec![
+///     (1, 1),
+///     (2, 1), (2, 2), (2, 3),
+///     (3, 1),
+/// ]);
+/// ```
+pub fn gen_position_map(s: &str) -> Vec<(usize, usize)> {
+    let mut v = vec![(0, 0); s.len()];
+    for (i, line) in s.lines().enumerate() {
+        let begin = ptr_diff(line.as_ptr(), s.as_ptr()) as usize;
+        for j in 0..line.len()+2 { // if ends with `\r\n`
+            v.get_mut(begin + j).map(|c| *c = (i + 1, j + 1));
+        }
+    }
+    v
+}
