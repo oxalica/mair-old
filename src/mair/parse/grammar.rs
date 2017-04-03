785bc9187c48162c93e49681f5c7720c862e23c3
uHOOCCOOHu <hooccooh1896@gmail.com>
2017-04-03 17:29:58 +0800

    Rewrite and finish item parsers. Remove tokenizer and mix parsers and lexer. Fix some error in rules of literals. Compact the rules.

--- src/mair/parse/grammar.rs
+++ src/mair/parse/grammar.rs
@@ -1,75 +1,121 @@
 use pest::prelude::*;
 
 impl_rdp! {
-    grammar! {
-        alpha    = _{ ['a'..'z'] | ['A'..'Z'] | ["_"] }
-        nl       = _{ ["\r\n"] | ["\n"] | ["\r"] } // \f \v
-        sp       = _{ [" "] | ["\t"] }
-        ident    = @{ alpha ~ (alpha | ['0'..'9'])* ~ t_ }
-        lifetime = @{ ["'"] ~ ident }
-        t_       = _{ !(alpha | ['0'..'9']) } // end of ident
+grammar! {
+    // basic chars
+    alpha    = _{ ['a'..'z'] | ['A'..'Z'] | ["_"] }
+    nl       = _{ ["\r\n"] | ["\n"] | ["\r"] } // \f \v
+    sp       = _{ [" "] | ["\t"] }
+    ident    = @{ alpha ~ (alpha | ['0'..'9'])* ~ t_ }
+    lifetime = @{ ["'"] ~ ident }
+    t_       = _{ !(alpha | ['0'..'9']) } // end of ident
 
-        // digit
-        bin      = _{ ['0'..'1'] }
-        oct      = _{ ['0'..'7'] }
-        dec      = _{ ['0'..'9'] }
-        hex      = _{ ['0'..'9'] | ['A'..'F'] | ['a'..'f'] }
+    // digit
+    bin      = _{ ['0'..'1'] }
+    oct      = _{ ['0'..'7'] }
+    dec      = _{ ['0'..'9'] }
+    hex      = _{ ['0'..'9'] | ['A'..'F'] | ['a'..'f'] }
 
-        // symbols
-        symbol   = {
-            ["::"] | ["->"] | ["=>"] |
-            ["=="] | ["!="] | ["<="] | [">="] |
-            ["&&"] | ["||"] |
-            ["!"] | ["="] |
-            ["+="] | ["-="] | ["*="] | ["/="] | ["%="] |
-            ["&="] | ["|="] | ["^="] | ["<<="] | [">>="] |
-            ["+"] | ["-"] | ["*"] | ["/"] | ["%"] |
-            ["&"] | ["|"] | ["^"] | ["<<"] | [">>"] |
-            ["<"] | [">"] |
-            ["$"] | ["#"] | [","] | [";"] | [":"] | ["."] | ["?"] | ["@"]
-        }
+    // symbols
+    operator = {
+        ["=="] | ["!="] | ["<="] | [">="] |
+        ["&&"] | ["||"] |
+        ["!"] | ["="] |
+        ["+="] | ["-="] | ["*="] | ["/="] | ["%="] |
+        ["&="] | ["|="] | ["^="] | ["<<="] | [">>="] |
+        ["+"] | ["-"] | ["*"] | ["/"] | ["%"] |
+        ["&"] | ["|"] | ["^"] | ["<<"] | [">>"] |
+        ["<"] | [">"] |
+        ["?"]
+    }
+    symbol = {
+        ["::"] | ["->"] | ["=>"] |
+        ["$"] | ["#"] | [","] | [";"] | [":"] | ["."] | ["@"]
+    }
 
-        // comments & whitespaces
-        line_comment       = _{
-            ["//"] ~ (!["/"] ~ !["!"] | ["//"]) ~ line_comment_tail
-        }
-        block_comment      = _{
-            ["/*"] ~ (!["*"] ~ !["!"] | ["**"]) ~ block_comment_tail
-        }
-        line_comment_tail  = _{ (!nl ~ any)* ~ nl }
-        block_comment_tail = _{
-            (!["/*"] ~ !["*/"] ~ any | block_comment)* ~ ["*/"]
-        }
-        comment    = _{ line_comment | block_comment }
-        whitespace = _{ nl | sp }
+    // keywords
+    kw_abstract = @{ ["abstract"] ~ t_ }
+    kw_alignof  = @{ ["alignof"]  ~ t_ }
+    kw_as       = @{ ["as"]       ~ t_ }
+    kw_become   = @{ ["become"]   ~ t_ }
+    kw_box      = @{ ["box"]      ~ t_ }
+    kw_break    = @{ ["break"]    ~ t_ }
+    kw_const    = @{ ["const"]    ~ t_ }
+    kw_continue = @{ ["continue"] ~ t_ }
+    kw_crate    = @{ ["crate"]    ~ t_ }
+    kw_do       = @{ ["do"]       ~ t_ }
+    kw_else     = @{ ["else"]     ~ t_ }
+    kw_enum     = @{ ["enum"]     ~ t_ }
+    kw_extern   = @{ ["extern"]   ~ t_ }
+    kw_false    = @{ ["false"]    ~ t_ }
+    kw_final    = @{ ["final"]    ~ t_ }
+    kw_fn       = @{ ["fn"]       ~ t_ }
+    kw_for      = @{ ["for"]      ~ t_ }
+    kw_if       = @{ ["if"]       ~ t_ }
+    kw_impl     = @{ ["impl"]     ~ t_ }
+    kw_in       = @{ ["in"]       ~ t_ }
+    kw_let      = @{ ["let"]      ~ t_ }
+    kw_loop     = @{ ["loop"]     ~ t_ }
+    kw_macro    = @{ ["macro"]    ~ t_ }
+    kw_match    = @{ ["match"]    ~ t_ }
+    kw_mod      = @{ ["mod"]      ~ t_ }
+    kw_move     = @{ ["move"]     ~ t_ }
+    kw_mut      = @{ ["mut"]      ~ t_ }
+    kw_offsetof = @{ ["offsetof"] ~ t_ }
+    kw_override = @{ ["override"] ~ t_ }
+    kw_priv     = @{ ["priv"]     ~ t_ }
+    kw_proc     = @{ ["proc"]     ~ t_ }
+    kw_pub      = @{ ["pub"]      ~ t_ }
+    kw_pure     = @{ ["pure"]     ~ t_ }
+    kw_ref      = @{ ["ref"]      ~ t_ }
+    kw_return   = @{ ["return"]   ~ t_ }
+    kw_big_self = @{ ["Self"]     ~ t_ }
+    kw_self     = @{ ["self"]     ~ t_ }
+    kw_sizeof   = @{ ["sizeof"]   ~ t_ }
+    kw_static   = @{ ["static"]   ~ t_ }
+    kw_struct   = @{ ["struct"]   ~ t_ }
+    kw_super    = @{ ["super"]    ~ t_ }
+    kw_trait    = @{ ["trait"]    ~ t_ }
+    kw_true     = @{ ["true"]     ~ t_ }
+    kw_type     = @{ ["type"]     ~ t_ }
+    kw_typeof   = @{ ["typeof"]   ~ t_ }
+    kw_unsafe   = @{ ["unsafe"]   ~ t_ }
+    kw_unsized  = @{ ["unsized"]  ~ t_ }
+    kw_use      = @{ ["use"]      ~ t_ }
+    kw_virtual  = @{ ["virtual"]  ~ t_ }
+    kw_where    = @{ ["where"]    ~ t_ }
+    kw_while    = @{ ["while"]    ~ t_ }
+    kw_yield    = @{ ["yield"]    ~ t_ }
 
-        // tokenize
-        tokens     = { whitespace* ~ token* }
-        token      = {
-            delimited |
-            doc |
-            ident | symbol | literal | lifetime
-        }
-        delimited  = {
-            ["("] ~ tokens ~ [")"] |
-            ["["] ~ tokens ~ ["]"] |
-            ["{"] ~ tokens ~ ["}"]
-        }
+    // comments & whitespaces
+    line_comment       = _{ ["//"] ~ (!["/"] ~ !["!"] | ["//"]) ~ line_comment_tail }
+    block_comment      = _{ ["/*"] ~ (!["*"] ~ !["!"] | ["**"]) ~ block_comment_tail }
+    line_comment_tail  = _{ (!nl ~ any)* ~ nl }
+    block_comment_tail = _{ (!["/*"] ~ !["*/"] ~ any | block_comment)* ~ ["*/"] }
+    comment            = _{ line_comment | block_comment }
+    whitespace         = _{ nl | sp }
 
-        // docs
-        doc       = { outer_doc | inner_doc }
-        outer_doc = {
-            ["///"] ~ !["/"] ~ line_comment_tail |
-            ["/**"] ~ !["*"] ~ block_comment_tail
-        }
-        inner_doc = {
-            ["//!"] ~ line_comment_tail |
-            ["/*!"] ~ block_comment_tail
-        }
+    // docs
+    outer_doc = {
+        ["///"] ~ !["/"] ~ line_comment_tail |
+        ["/**"] ~ !["*"] ~ block_comment_tail
+    }
+    inner_doc = {
+        ["//!"] ~ line_comment_tail |
+        ["/*!"] ~ block_comment_tail
+    }
 
-        // literals
-        literal    = { char_lit | string_lit | int_lit | float_lit }
+    // attributes
+    outer_attr =  { outer_doc | ["#"] ~ ["["] ~ attr_obj ~ ["]"] }
+    inner_attr =  { inner_doc | ["#"] ~ ["!"] ~ ["["] ~ attr_obj ~ ["]"] }
+    attr_obj   =  {
+        ident ~ ["="] ~ attr_value |
+        ident ~ (["("] ~ (attr_obj ~ ([","] ~ attr_obj)* ~ [","]?)? ~ [")"])?
+    }
+    attr_value = _{ literal }
 
+    // >> literals
+    literal = { char_lit | bchar_lit | string_lit | bstring_lit | int_lit | float_lit }
         // number literals
         int_lit      = @{ (
             ["0b"] ~ (bin | ["_"])+ |
@@ -108,48 +154,86 @@ impl_rdp! {
             hex ~ (hex ~ (hex ~ (hex ~ (hex ~ hex?)?)?)?)? ~
             ["}"]
         }
+    // << literals
 
-        // items
-        access        =  { (["pub"] ~ t_)? }
-        item_ext_crt  =  {
-            ["extern"] ~ t_ ~ ["crate"] ~ t_ ~ ident ~
-            (["as"] ~ t_ ~ ident)? ~ [";"]
-        }
-        item_use      =  { access ~ ["use"] ~ t_ ~ use_path ~ [";"] }
-        item_mod      =  {
-            access ~ ["mod"] ~ t_ ~ ident ~ ["{"] ~ ["}"]  // TODO: module inner
-        }
-        item_fn       =  {
-            access ~ ["fn"] ~ t_ ~ ident ~ template? ~
-            ["("] ~ (fn_arg ~ ([","] ~ fn_arg)*)? ~ [")"] ~
-            ["{"] ~ fn_inner ~ ["}"]
-        }
-        use_path      =  { ident ~ (["::"] ~ ident)* ~ (
-            ["*"] |
-            ["{"] ~ ident ~ ([","] ~ ident)* ~ ["}"]
+    // >> crate(module) & items
+    module            =  { inner_attr* ~ item* }
+    vis               =  { kw_pub? }
+    item              =  { outer_attr* ~ vis ~ (
+        item_extern_crate |
+        item_use |
+        item_mod |
+        item_fn |
+        item_extern |
+        item_type |
+        item_struct |
+        item_enum |
+        item_const |
+        iten_static |
+        item_trait |
+        item_impl
+    ) }
+    item_extern_crate  =  { kw_extern ~ kw_crate ~ ident ~ (kw_as ~ ident)? ~ [";"] }
+    item_use           =  { kw_use ~ use_path ~ [";"] }
+        use_path       =  { ident ~ (["::"] ~ ident)* ~ (
+            kw_as ~ ident |
+            ["::"] ~ ["*"] |
+            ["::"] ~ ["{"] ~ (ident ~ ([","] ~ ident)* ~ [","]?)? ~ ["}"]
         )? }
-        fn_arg        =  { ident ~ [":"] ~ ty } // TODO: tuple match
-        fn_inner      =  { any* } // TODO
-        // TODO: other items
-        template      =  { (
-            ["<"] ~
-            (lifetime ~ ([","] ~ lifetime)* | ident) ~ ([","] ~ ident)* ~
-            [">"]
+    item_mod           =  { kw_mod ~ ident ~ ["{"] ~ module ~ ["}"] }
+    item_fn            =  { fn_head ~ block_expr }
+        fn_head        =  {
+            kw_fn ~ ident ~ template? ~
+            ["("] ~ (fn_arg ~ ([","] ~ fn_arg)* ~ [","]?)? ~ [")"] ~ (["->"] ~ fn_ret)? ~
+            where_clause?
+        }
+        fn_ret         =  { ["!"] | ty }
+        fn_arg         =  { ident ~ [":"] ~ ty } // TODO: tuple match
+        template       =  { (
+            ["<"] ~ (lifetime ~ ([","] ~ lifetime)* | ident) ~ ([","] ~ ident)* ~ [">"]
         )? }
-
-        // ty
-        ty = { ident } // TODO: other composite types
+        where_clause   =  { kw_where ~ (ty ~ [":"] ~ trait_name)+ }
+    item_extern        =  { kw_extern ~ string_lit? ~ ["{"] ~ fn_extern_decl* ~ ["}"] }
+        fn_extern_decl =  { inner_attr* ~ fn_head ~ [";"] }
+    item_type          =  { kw_type ~ ident ~ template? ~ ["="] ~ ty ~ [";"] }
+    item_struct        =  { kw_struct ~ ident ~ template? ~ (
+        [";"] |
+        ty_tuple ~ [";"] |
+        struct_body
+    ) }
+        struct_body    =  {
+            ["{"] ~ (struct_field ~ ([","] ~ struct_field)* ~ [","]*)? ~ ["}"]
+        }
+        struct_field   =  { vis ~ ident ~ [":"] ~ ty }
+    item_enum          =  { kw_enum ~ ident ~ template? ~
+        ["{"] ~ (enum_field ~ ([","] ~ enum_field)* ~ [","]*)? ~ ["}"]
     }
-}
+        enum_field     =  { ident ~ (ty_tuple | struct_body)? }
+    item_const         =  { kw_const ~ ident ~ [":"] ~ ty ~ ["="] ~ const_expr }
+    iten_static        =  { kw_static ~ ident ~ [":"] ~ ty ~ ["="] ~ const_expr }
+    item_trait         =  {
+        kw_trait ~ ident ~ template? ~
+        ["{"] ~ (trait_decl ~ ([","] ~ trait_decl)* ~ [","]?)? ~ ["}"]
+    }
+        trait_decl     =  { trait_decl_ty | trait_decl_fn }
+        trait_decl_ty  =  { kw_type ~ ident ~ (["="] ~ ty)? ~ [";"] }
+        trait_decl_fn  =  { fn_head ~ ([";"] | ["{"] ~ inner_attr* ~ block_expr ~ ["}"]) }
+    item_impl          =  {
+        kw_impl ~ template? ~ (ty | trait_name ~ kw_for ~ ty) ~
+        ["{"] ~ (item_type | item_fn)* ~ ["}"]
+    }
+    // << crate(module) & items
 
-#[test]
-fn test_tokenize() {
-    let mut parser = Rdp::new(StringInput::new(r##"
-fn main() {
-    println!("hello {}", "world");
-}
-    "##));
-    println!("{} {}", parser.tokens(), parser.end());
-    println!("{:?}", parser.queue());
-    assert!(false);
-}
+    // type & trait_name
+    ty         =  { ident } // TODO: other composite types
+    ty_tuple   =  { ["("] ~ (ty ~ ([","] ~ ty)* ~ [","]?)? ~ [")"] }
+    trait_name =  { ident ~ (["<"] ~ ty ~ ([","] ~ ty)* ~ [","]? ~ [">"])? }
+
+    // expressions
+    const_expr =  { any* } // TODO
+    block_expr =  { any* } // TODO
+
+    // crate
+    crate_file = _{ whitespace* ~ module ~ eoi }
+} // grammar!
+} // impl_rdp!
