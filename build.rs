b2f542d4f04ca4d21b5b79e5c4dd2c66284052f7
uHOOCCOOHu <hooccooh1896@gmail.com>
2017-06-08 09:28:12 +0800

    Fix `Cargo.toml`. Add clippy lints for lib. Move files generated by lalrpop to `target/`.

--- build.rs
+++ build.rs
@@ -1,5 +1,9 @@
 extern crate lalrpop;
 
 fn main() {
-    lalrpop::process_root().unwrap();
+    lalrpop::Configuration::new()
+        .use_colors_if_tty()
+        .use_cargo_dir_conventions()
+        .process()
+        .unwrap();
 }
