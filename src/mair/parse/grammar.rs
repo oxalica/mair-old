83eae255a351e47ccbf5e8dd2f6b8fd6520aefa1
uHOOCCOOHu <hooccooh1896@gmail.com>
2017-06-20 18:08:45 +0800

    Write module parser and modify ast types. Wrap generated parser in a sub mod. Simplify.

--- src/mair/parse/grammar.rs
+++ src/mair/parse/grammar.rs
@@ -1,4 +1,25 @@
-#![cfg_attr(feature="clippy", allow(clippy))]
-#![allow(unused_imports)]
+pub use self::parser::{
+    parse_tts,
+    parse_tt,
+    parse_module,
+    parse_item,
+    parse_ty,
+};
 
-include!(concat!(env!("OUT_DIR"), "/mair/parse/grammar.rs"));
+use super::ast::{Path, Template, TraitBound};
+
+fn is_placeholder<'a>(x: &Path<'a>) -> bool {
+    x.comps == vec![super::ast::PathComp{ body: "_", hint: None }]
+}
+
+fn merge_templ<'a>(mut templ: Template<'a>, mut whs: Vec<TraitBound<'a>>) -> Template<'a> {
+    templ.trait_bounds.append(&mut whs);
+    templ
+}
+
+mod parser {
+    #![cfg_attr(feature="clippy", allow(clippy))]
+    #![allow(unused_imports)]
+
+    include!(concat!(env!("OUT_DIR"), "/mair/parse/grammar.rs"));
+}
